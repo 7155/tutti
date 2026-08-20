a52163f1a48b8db8a325a9127cf6c65d0b18fc68
jomeswang <1551403343@qq.com>
2026-08-20 17:18:50 +0800

    refactor(agent-gui): keep detail pane within budget
    Signed-off-by: jomeswang <1551403343@qq.com>

--- packages/agent/gui/agent-gui/agentGuiNode/view/AgentGUIDetailPane.tsx
+++ packages/agent/gui/agent-gui/agentGuiNode/view/AgentGUIDetailPane.tsx
@@ -303,7 +303,6 @@ export const AgentGUIDetailPane = memo(function AgentGUIDetailPane({
     submitInteractivePrompt,
     dismissBottomDockPrompt
   );
-  const isInteractionPending = activePromptResponsePending;
   const composerActivePromptDisabledReason =
     resolveAgentGUIComposerInteractionDisabledReason(
       composerActivePrompt?.kind,
@@ -475,7 +474,7 @@ export const AgentGUIDetailPane = memo(function AgentGUIDetailPane({
             }
           : null,
       isSendingTurn: isComposerSending,
-      isSubmittingPrompt: isInteractionPending,
+      isSubmittingPrompt: activePromptResponsePending,
       uiLanguage,
       labels: composerLabels,
       workspaceUserProjectI18n,
@@ -611,7 +610,7 @@ export const AgentGUIDetailPane = memo(function AgentGUIDetailPane({
       viewModel.composer.isTuttiModeUpdating,
       viewModel.composer.tuttiModeEffect,
       viewModel.composer.tuttiModeSpeed,
-      isInteractionPending,
+      activePromptResponsePending,
       viewModel.composer.promptImagesSupported,
       viewModel.composer.queueStatus,
       viewModel.composer.queuedPrompts,
@@ -653,7 +652,7 @@ export const AgentGUIDetailPane = memo(function AgentGUIDetailPane({
     viewModel.composer.queuedPrompts.map((prompt) => prompt.id).join(","),
     viewModel.composer.queueStatus,
     viewModel.composer.drainingQueuedPromptId ?? "",
-    isInteractionPending ? "1" : "0"
+    activePromptResponsePending ? "1" : "0"
   ].join("|");
   const {
     followEndMode,
@@ -692,7 +691,7 @@ export const AgentGUIDetailPane = memo(function AgentGUIDetailPane({
           : undefined
       }
       noticeChrome={homeNoticeChrome}
-      isRespondingApproval={isInteractionPending}
+      isRespondingApproval={activePromptResponsePending}
       onSubmitApprovalOption={submitApproval}
       onRetryActivation={retryActivation}
       onAuthLogin={authLogin}
@@ -771,7 +770,7 @@ export const AgentGUIDetailPane = memo(function AgentGUIDetailPane({
                 viewModel.interaction.interactivePromptDisabledReason
               }
               inlineNoticeChrome={inlineNoticeChrome}
-              isRespondingApproval={isInteractionPending}
+              isRespondingApproval={activePromptResponsePending}
               sessionChrome={sessionChrome}
               keyboardShortcutsEnabled={isActive && !sideComposerFocused}
               chromeLabels={chromeLabels}
